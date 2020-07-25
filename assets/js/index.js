8c793ca6354010e607ddfe856ab24baacc6fc330
steven97cr <steven97cr@outlook.com>
2020-07-24 22:10:01 -0600

    Result page and validations

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ function main(data, callback){
         callback: null,
 
         init: function(data, callback){
-            cl.data = data;
+            cl.page = data.currentPage;
             cl.callback = callback;
             cl.listeners();
             cl.configPage();
@@ -14,16 +14,45 @@ function main(data, callback){
 
         configPage: function(){
             if(cl.page === "result"){
+                console.log('result')
                 if(localStorage.getItem('requestResult')){
                     cl.setResultInfo(JSON.parse(localStorage.getItem('requestResult')))
+                    localStorage.clear();
                 }else{
-                    //show error not found
+                    document.querySelector("#searchResultContainer").style.display = "none";
+                    document.querySelector("#searchErrorContainer").style.display = "block";
                 }
             }
         },
 
         setResultInfo: function(data){
-            document.querySelector("#userPhoto").innerHTML = ""
+            document.querySelector("#userPhoto").style.backgroundImage = "url('"+(data.image ? data.image : '../../assets/img/userDefault.png')+"')";
+            document.querySelector("#userName h2").innerHTML = (data.first_name +" "+data.last_name); 
+            document.querySelector("#userName span").innerHTML = data.description ? data.description : '';
+            document.querySelector("#userInfoAddressSection").innerHTML += data.address ? ("<p class='userInfoText'>"+data.address+"<p>") : 'Not provided';
+            document.querySelector("#userInfoEmailSection").innerHTML += data.email ? ("<p class='userInfoText'>"+data.email+"<p>") : 'Not provided';
+
+            document.querySelector("#userInfoPhoneSection").innerHTML += data.phone_numbers ? cl.getPhonesHtml(data.phone_numbers) : 'Not provided';
+            document.querySelector("#userInfoRelativesSection").innerHTML += data.relatives ? cl.getRelativesHtml(data.relatives) : 'Not provided';
+        },
+
+        getPhonesHtml: function(phones){
+            var html = '';
+                console.log(phones)
+            for(var i = 0; i < phones.length; i++){
+                html += "<a href='tel:"+phones[i]+"'>"+phones[i]+"</a><br>";
+            }
+                
+            return html;
+        },
+
+        getRelativesHtml: function(relatives){
+            var html = '';
+                console.log(relatives)
+            for(var i=0; i < relatives.length; i++){
+                html += "<p class='userInfoText'>"+relatives[i]+"</p>";
+            }
+            return html;
         },
 
         toggleElement: function(selector){
@@ -37,23 +66,48 @@ function main(data, callback){
               
         },
 
-        listeners: function(){
+        valitateEmailInput: function(selector){
+            var out = false;
+            var input = document.querySelector(selector);
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value)){
+                out = true;
+            }else{
+                input.style.border = "1px red solid";
+                document.querySelector(".spanTextError").style.display = "block";
+                setTimeout(function(){
+                    input.style.border = "1px whitesmoke solid";
+                    document.querySelector(".spanTextError").style.display = "none";
+                },4000)
+            }
+            return out;
+        },
 
-            document.querySelector("#submitFormBtn").addEventListener("click", function(e){
-                //cl.toggleElement("#searchForm");
-                //cl.toggleElement("#formLoader");
-                e.preventDefault();
-                cl.makeHttpRequest('GET', 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=doesmith@example.com', {}, 
+        searchEmail: function(){
+            if(cl.valitateEmailInput("#searchInput")){
+                var val = document.querySelector("#searchInput").value;
+                cl.makeHttpRequest('GET', 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email='+val, {}, 
                 function(response){
                     if(response != "[]"){
                         console.log(response);
                         localStorage.setItem('requestResult', response);
-                        window.location.href = './src/result'
+                        if(cl.page == "result"){
+                            document.location.reload();
+                        }else{
+                            window.location.href = './src/result';
+                        }
                     }
                 },
                 function(err){
                     console.log(err)
                 })
+            }
+        },
+
+        listeners: function(){
+
+            document.querySelector("#submitFormBtn").addEventListener("click", function(e){
+                e.preventDefault();
+                cl.searchEmail();
             });
               
         }, 
