f3b13067a34f9c5c5fe0c99af7aec4ccb2762682
steven97cr <steven97cr@outlook.com>
2020-07-24 18:06:07 -0600

    Request result saved into local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,22 +2,54 @@ function main(data, callback){
 
     var cl = {
 
-        data: null,
+        page: null,
         callback: null,
 
         init: function(data, callback){
             cl.data = data;
             cl.callback = callback;
             cl.listeners();
+            cl.configPage();
+        },
+
+        configPage: function(){
+            if(cl.page === "result"){
+                if(localStorage.getItem('requestResult')){
+                    cl.setResultInfo(JSON.parse(localStorage.getItem('requestResult')))
+                }else{
+                    //show error not found
+                }
+            }
+        },
+
+        setResultInfo: function(data){
+            document.querySelector("#userPhoto").
+        },
+
+        toggleElement: function(selector){
+            
+            var el = document.querySelector(selector);
+            if (el.style.display === "none") {
+                el.style.display = "block";
+            } else {
+                el.style.display = "none";
+            }
+              
         },
 
         listeners: function(){
 
             document.querySelector("#submitFormBtn").addEventListener("click", function(e){
+                //cl.toggleElement("#searchForm");
+                //cl.toggleElement("#formLoader");
                 e.preventDefault();
                 cl.makeHttpRequest('GET', 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=doesmith@example.com', {}, 
                 function(response){
-                    console.log(response)
+                    if(response != "[]"){
+                        console.log(response);
+                        localStorage.setItem('requestResult', response);
+                        window.location.href = './src/result'
+                    }
                 },
                 function(err){
                     console.log(err)
